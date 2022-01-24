98d38901ab362f97f1f8e3511aec3953f6008d2f
Shahin <majd.shahin.ext@siemens.com>
2022-01-24 13:29:44 +0100

    a

--- PYP/19pyth10/PyPrgs/b01/hello.py
+++ PYP/19pyth10/PyPrgs/b01/hello.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 import sys
 
-def main(arg):
+# def main(arg):
+#     "Hello"
+#     print("Hello Python World!",arg)
+    
+
+
+# if __name__ == '__main__':
+#     main(sys.argv)
+def main():
     "Hello"
-    print("Hello Python World!",arg)
+    print("Hello Python World!")
     
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
---
+++ PYP/PYP_2022/01/a.py
@@ -0,0 +1,8 @@
+import sys
+def __main__(arg):
+    n=len(arg)
+    for a in arg:
+        print(a,"\n")
+
+if __name__ == "__main__":
+    __main__(sys.argv)
